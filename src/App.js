96f2b99907fb337e32121874255142e1408fe8d7
Marcelo <marcelo.cansilva@gmail.com>
2021-11-20 17:17:58 -0300

    Create page teste

--- src/App.js
+++ src/App.js
@@ -1,11 +1,67 @@
+import {useEffect, useState} from "react";
 import './App.css';
+import api from "./services/api";
+import "./styles.css";
 
 function App() {
+  const [products, setProducts] = useState([])
+  async function fetchData() {
+    const response = await api.get("products")
+    setProducts(response.data);
+    console.log(response.data);
+  }
+  useEffect(() => {fetchData()}, [])
   return (
     <div className="App">
-      <header className="App-header">
-        Hello World!
-      </header>
+      <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+      <form className="d-flex">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+      </div>
+      <h1>Viagens disponíveis</h1>
+      {products.map((product)=>{
+        return(
+          <div className="">
+            <div>
+              <h1>{product.title}</h1>
+              <h2>{product.subtitle}</h2>
+            </div>
+          </div>
+        )
+      })}
     </div>
   );
 }
